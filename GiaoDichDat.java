8ae0b3b1530f52c129cb333d218402a0f8c384ae
baokhang2003 <1050080102@sv.hcmunre.edu.vn>
2023-05-24 14:58:37 +0700

    HDT

---
+++ GiaoDichDat.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labhdtt4;
+
+/**
+ *
+ * @author phankhang
+ */
+public class GiaoDichDat extends GiaoDich {
+    private String loaiDat;
+ 
+    public GiaoDichDat() {
+        super();
+    }
+ 
+    public GiaoDichDat(String loaiDat) {
+        super();
+        this.loaiDat = loaiDat;
+    }
+ 
+    public String getLoaiDat() {
+        return loaiDat;
+    }
+ 
+    public void setLoaiDat(String loaiDat) {
+        this.loaiDat = loaiDat;
+    }
+     
+    public void nhap() {
+        super.nhap();
+        System.out.print("Nhập loại đất (A/B/C): ");
+        loaiDat = scanner.nextLine();
+    }
+ 
+    @Override
+    public String toString() {
+        return super.toString() + ", loại đất: " + this.loaiDat;
+    }
+}
